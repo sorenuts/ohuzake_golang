0044f18407c922f46bd6a6866c4ecc190b89f76e
うにナッツ <15rd031@ms.dendai.ac.jp>
2018-06-24 20:29:44 +0900

    恥ずかしいミスの訂正

--- vanilla.go
+++ vanilla.go
@@ -7,15 +7,28 @@ import(
 	"reflect"
 )
 
-StrCall := []string{"V!","a!","n!","i!","l!","la!"}
+//掛け声の方の「コール」
+var StrFirstCall = []string{"V!","a!","n!","i!","l!","la!"}
+var StrSecondCall = []string{"バーニラ","バニラ","バニラで"}
+//コールの順番の定義
+var SequenceStage3 = []int{0, 1, 0}
+var SequenceStage4 = []int{0, 1}
+var SequenceStage6 = []int{0, 2}
 
 func main(){
 	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("[Stage:1]")
 	stage1()
+	fmt.Println("[Stage:2]")
 	stage2()
+	fmt.Println("[Stage:3]")
 	stage3()
+	fmt.Println("[Stage:4]")
 	stage4()
+	fmt.Println("[Stage:5]")
 	stage5()
+	fmt.Println("[Stage:6]")
 	stage6()
 }
 
@@ -23,14 +36,12 @@ func main(){
 * Stage1:"V!a!n!i!l!la!"文字列完成で「バニラ！」
 */
 func stage1(){
-	fmt.Println("[Stage:1]")
-
 	x, stage := 0, 0
 
 	//Vanilla文字列完成シーケンス
-	for stage != len(StrCall){
-		x = rand.Intn(len(StrCall))
-		fmt.Print(StrCall[x]," ")
+	for stage != len(StrFirstCall){
+		x = rand.Intn(len(StrFirstCall))
+		fmt.Print(StrFirstCall[x]," ")
 		if x == stage{
 			stage++
 		}else{
@@ -46,7 +57,6 @@ func stage1(){
 * Stage2:Stage1と同じ
 */
 func stage2(){
-	fmt.Println("[Stage:2]")
 	stage1()
 }
 
@@ -54,14 +64,12 @@ func stage2(){
 * 「バーニラ　バニラ　バーニラ」文字列完成で「求人」
 */
 func stage3(){
-	str := []string{"バーニラ","バニラ","バニラで"}
 	//x:乱数の格納, sequence:直近3回の乱数の履歴
 	x, sequence := 0, make([]int,3)
-	SequenceStage3 := []int{0, 1, 0}
 
 	for reflect.DeepEqual(sequence, SequenceStage3) == false{
-		x = rand.Intn(len(str))
-		fmt.Print(str[x]," ")
+		x = rand.Intn(len(StrSecondCall))
+		fmt.Print(StrSecondCall[x]," ")
 		if len(sequence) == len(SequenceStage3){
 			sequence = sequence[1:]
 		}
@@ -75,14 +83,12 @@ func stage3(){
 * 「バーニラ　バニラ」文字列完成で「高収入」
 */
 func stage4(){
-	str := []string{"バーニラ","バニラ","バニラで"}
 	//x:乱数の格納, sequence:直近2回の乱数の履歴
 	x, sequence := 0, make([]int,2)
-	SequenceStage4 := []int{0, 1}
 
 	for reflect.DeepEqual(sequence, SequenceStage4) == false{
-		x = rand.Intn(len(str))
-		fmt.Print(str[x]," ")
+		x = rand.Intn(len(StrSecondCall))
+		fmt.Print(StrSecondCall[x]," ")
 		if len(sequence) == len(SequenceStage4){
 			sequence = sequence[1:]
 		}
@@ -97,14 +103,12 @@ func stage5(){
 }
 
 func stage6(){
-	str := []string{"バーニラ","バニラ","バニラで"}
 	//x:乱数の格納, sequence:直近2回の乱数の履歴
 	x, sequence := 0, make([]int,2)
-	SequenceStage6 := []int{0, 2}
 
 	for reflect.DeepEqual(sequence, SequenceStage6) == false{
-		x = rand.Intn(len(str))
-		fmt.Print(str[x]," ")
+		x = rand.Intn(len(StrSecondCall))
+		fmt.Print(StrSecondCall[x]," ")
 		if len(sequence) == len(SequenceStage6){
 			sequence = sequence[1:]
 		}
